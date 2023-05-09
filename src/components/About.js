8c55bee85e86b355d3d7d3d3e583bc13c100f378
malcomowilla <owillamalcom@gmail.com>
2023-05-09 14:17:38 +0300

    solveed

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,42 @@
 import React from "react";
 import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+function About(){
+
+ 
+  return(
+   <div id="about">
+  <h2>About me</h2>
+  <p>am a softaware engineer some months into the program, nearing completion 😌😎</p>
+  <img src={image} alt="i made this"></img>
+</div>
+  )
 }
 
+//function About() {
+  //return <div id="about">About</div>
+  
+
+//}
+
+
 export default About;
+
+
+ 
+
+
+
+
+
+
+
+//function about(){
+
+ //return <div>
+  //<div id="about">about</div>
+  //<h2>About me</h2>
+  //<p>am a softaware engineer some months into the program</p>
+  //<img src="https://i.imgur.com/mV8PQxj.gif" alt="i made this"></img>
+//</div>
+//}
